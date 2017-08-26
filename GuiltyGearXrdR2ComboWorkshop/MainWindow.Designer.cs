6fec44ba9849639a2c06c303ab0933e2e388f307
Tom Solacroup <tom.solacroup@gmail.com>
2017-08-26 14:33:14 +0200

    Initial commit

---
+++ GuiltyGearXrdR2ComboWorkshop/MainWindow.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace GGXrdR2ComboWorkshop
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInject = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInject
+            // 
+            this.btnInject.Location = new System.Drawing.Point(72, 96);
+            this.btnInject.Name = "btnInject";
+            this.btnInject.Size = new System.Drawing.Size(75, 23);
+            this.btnInject.TabIndex = 0;
+            this.btnInject.Text = "Inject";
+            this.btnInject.UseVisualStyleBackColor = true;
+            this.btnInject.Click += new System.EventHandler(this.btnInject_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnInject);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInject;
+    }
+}
+
---
+++ GuiltyGearXrdR2ComboWorkshop/MainWindow.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyHook;
+using Capture.Interface;
+using Capture.Hook;
+using Capture;
+using System.Threading;
+
+namespace GGXrdR2ComboWorkshop
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInject_Click(object sender, EventArgs e)
+        {
+            if (_captureProcess == null)
+            {
+                btnInject.Enabled = false;
+
+                AttachProcess();
+            }
+            else
+            {
+                HookManager.RemoveHookedProcess(_captureProcess.Process.Id);
+                _captureProcess.CaptureInterface.Disconnect();
+                _captureProcess = null;
+            }
+
+            if (_captureProcess != null)
+            {
+                btnInject.Text = "Detach";
+                btnInject.Enabled = true;
+            }
+            else
+            {
+                btnInject.Text = "Inject";
+                btnInject.Enabled = true;
+            }
+        }
+
+        int processId = 0;
+        Process _process;
+        CaptureProcess _captureProcess;
+        private void AttachProcess()
+        {
+            string exeName = "GuiltyGearXrd";
+
+            Process[] processes = Process.GetProcessesByName(exeName);
+            Process[] processesDebug = Process.GetProcesses();
+            foreach (Process process in processesDebug)
+                System.Diagnostics.Debug.WriteLine(process.ProcessName);
+            foreach (Process process in processes)
+            {
+                // Simply attach to the first one found.
+
+                // If the process doesn't have a mainwindowhandle yet, skip it (we need to be able to get the hwnd to set foreground etc)
+                if (process.MainWindowHandle == IntPtr.Zero)
+                {
+                    continue;
+                }
+
+                // Skip if the process is already hooked (and we want to hook multiple applications)
+                if (HookManager.IsHooked(process.Id))
+                {
+                    continue;
+                }
+
+                Direct3DVersion direct3DVersion = Direct3DVersion.Direct3D10;
+                direct3DVersion = Direct3DVersion.AutoDetect;
+
+                CaptureConfig cc = new CaptureConfig()
+                {
+                    Direct3DVersion = direct3DVersion,
+                    ShowOverlay = true
+                    
+                };
+
+                processId = process.Id;
+                _process = process;
+
+                var captureInterface = new CaptureInterface();
+                captureInterface.RemoteMessage += new MessageReceivedEvent(CaptureInterface_RemoteMessage);
+                _captureProcess = new CaptureProcess(process, cc, captureInterface);
+
+                break;
+            }
+            Thread.Sleep(10);
+
+            if (_captureProcess == null)
+            {
+                MessageBox.Show("No executable found matching: '" + exeName + "'");
+            }
+        }
+
+        /// <summary>
+        /// Display messages from the target process
+        /// </summary>
+        /// <param name="message"></param>
+        void CaptureInterface_RemoteMessage(MessageReceivedEventArgs message)
+        {
+            /*
+            txtDebugLog.Invoke(new MethodInvoker(delegate ()
+            {
+                txtDebugLog.Text = String.Format("{0}\r\n{1}", message, txtDebugLog.Text);
+            })
+            );
+            */
+        }
+    }
+}
